54797be34afcb77541bf9b90c2ae1a2a01afaca3
Yu, Jack <jackingyu@hotmail.com>
2017-12-25 12:01:06 +0800

    no message

--- tools/dropdb.sql
+++ tools/dropdb.sql
@@ -1,2 +1,2 @@
-drop database test;
-CREATE DATABASE `test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; 
+drop database blaimar;
+CREATE DATABASE `blaimar` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; 
